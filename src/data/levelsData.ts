7c3808b8f1dd37d730fb163ef5d9af155c55bc5f
Ilya Kotsur <99085805+Ilyxxxaa@users.noreply.github.com>
2023-05-07 22:36:01 +0300

    feat: add levels buttons

---
+++ src/data/levelsData.ts
@@ -0,0 +1,32 @@
+export const levelsData = [
+  {
+    title: "easy",
+    level: "A1",
+    color: "#e050d9",
+  },
+  {
+    title: "easy",
+    level: "A2",
+    color: "#0095ff",
+  },
+  {
+    title: "medium",
+    level: "B1",
+    color: "rgb(158, 255, 195)",
+  },
+  {
+    title: "medium",
+    level: "B2",
+    color: "#e6e930",
+  },
+  {
+    title: "hard",
+    level: "C1",
+    color: "#FFAD00",
+  },
+  {
+    title: "hard",
+    level: "C2",
+    color: "#e8743a",
+  },
+];
--- src/pages/BookPage/BookPage.vue
+++ src/pages/BookPage/BookPage.vue
@@ -1,13 +1,100 @@
 <template>
-  <div>BookPage</div>
+  <div class="container">
+    <Header />
+    <div class="book_content">
+      <h2 class="title">Учебник</h2>
+      <div class="levels">
+        <button
+          v-for="(item, index) in levelsData"
+          :key="item.color"
+          class="levels-btn"
+          :style="{
+            'background-color':
+              index === activeLevel ? 'white' : 'rgba(255, 255, 255, 0.5)',
+          }"
+          @click="changeLevel(index)"
+        >
+          <div class="btn-left">{{ item.title }}</div>
+          <div class="btn-right">{{ item.level }}</div>
+          <span
+            class="btn-circle"
+            :style="{
+              'background-color': index === activeLevel ? item.color : 'white',
+            }"
+          />
+        </button>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
+import { Header } from "@/components";
+import { levelsData } from "@/data/levelsData";
 
 export default defineComponent({
   name: "BookPage",
+  components: { Header },
+  data() {
+    return {
+      levelsData,
+      activeLevel: 0,
+    };
+  },
+  methods: {
+    changeLevel(level: number) {
+      this.activeLevel = level;
+    },
+  },
 });
 </script>
 
-<style scoped></style>
+<style lang="scss">
+.book_content {
+  display: flex;
+  flex-direction: column;
+
+  .title {
+    color: white;
+    font-size: 32px;
+    font-weight: 900;
+    align-self: center;
+  }
+  .levels {
+    display: flex;
+    justify-content: space-between;
+    flex-wrap: wrap;
+    gap: 20px;
+    &-btn {
+      width: 170px;
+      height: 50px;
+      border-radius: 0.5rem;
+      color: #2c2647;
+      font-size: 20px;
+      font-weight: 900;
+      text-transform: uppercase;
+      display: flex;
+      justify-content: space-between;
+      position: relative;
+      align-items: center;
+      cursor: pointer;
+      overflow: hidden;
+      border: none;
+      padding: 10px;
+    }
+    .btn-right {
+      z-index: 2;
+    }
+
+    .btn-circle {
+      position: absolute;
+      border-radius: 50%;
+      height: 110px;
+      width: 110px;
+      top: 0;
+      right: -55px;
+    }
+  }
+}
+</style>
